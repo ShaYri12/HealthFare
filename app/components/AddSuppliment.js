8f9f49f0bb028c9fee469c753d1be2a1e7e6af97
ShaYri12 <sharjeelb60@gmail.com>
2024-06-15 04:09:14 +0500

    fix

--- app/components/AddSuppliment.js
+++ app/components/AddSuppliment.js
@@ -91,45 +91,64 @@ const StepThree = ({ handleOrignalStep, handleChange, values, cart2, setCart2 })
 
 
     return (
-      <div className='card card-supplement'>
-        <div className='card-top'>
-          <div className='card-img'>
+      <div className="card card-supplement">
+        <div className="card-top">
+          <div className="card-img">
             <img src={imgSrc} alt={title} />
           </div>
-          <div className='card-title-price title-price-stepthree'>
+          <div className="card-title-price title-price-stepthree">
             <h3>{title}</h3>
-            <div className='price-desc'>
+            <div className="price-desc">
               <h3>{price}</h3>
               <p>{desc}</p>
             </div>
           </div>
         </div>
         <div className="btn-group btn-group-stepthree">
-        <div className='quantity-control'>
-            <button className='quantity-btn quantity-increase' onClick={decreaseQuantity}>-</button>
+          <div className="quantity-control">
+            <button
+              className="quantity-btn quantity-increase"
+              onClick={decreaseQuantity}
+            >
+              -
+            </button>
             <span>{quantity}</span>
-            <button className='quantity-btn quantity-dicrease' onClick={increaseQuantity}>+</button>
+            <button
+              className="quantity-btn quantity-dicrease"
+              onClick={increaseQuantity}
+            >
+              +
+            </button>
+          </div>
+          {isInCart ? (
+            <div className="forward-btns">
+              <button
+                className="long-btn add-cart-btn"
+                onClick={handleAddToCart}
+              >
+                {quantityInCart} - In Cart
+              </button>
+              <button
+                className="arrow-btn cart-btn in-cart"
+                onClick={removeFromCart}
+              >
+                <img src="/assets/delete.svg" alt="cart" />
+              </button>
+            </div>
+          ) : (
+            <div className="forward-btns">
+              <button
+                className="long-btn add-cart-btn"
+                onClick={handleAddToCart}
+              >
+                Add to cart
+              </button>
+              <button className="arrow-btn cart-btn">
+                <img src="/assets/cart.svg" alt="cart" />
+              </button>
+            </div>
+          )}
         </div>
-        {isInCart ? (
-          <div className='forward-btns'>
-          <button className="long-btn add-cart-btn" onClick={handleAddToCart}>
-           {quantityInCart} - In Cart
-          </button>
-          <button className="arrow-btn cart-btn in-cart" onClick={removeFromCart}>
-            <img src="/assets/delete.svg" alt="cart" />
-          </button>
-        </div>
-        ):(
-        <div className='forward-btns'>
-          <button className="long-btn add-cart-btn" onClick={handleAddToCart}>
-            Add to cart
-          </button>
-          <button className="arrow-btn cart-btn">
-            <img src="/assets/cart.svg" alt="cart" />
-          </button>
-        </div>
-      )}
-      </div>
       </div>
     );
   };
